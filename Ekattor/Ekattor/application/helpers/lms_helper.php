86389cc8cdd6313b8fc4a6b1e5988938be4da8fb
Abhishek940 <akabhishek7485@gmail.com>
2021-07-21 16:48:26 +0530

    initial commit

--- Ekattor/Ekattor/application/helpers/lms_helper.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-/*
-*  @author   : Creativeitem
-*  date      : November, 2019
-*  Ekattor School Management System With Addons
-*  http://codecanyon.net/user/Creativeitem
-*  http://support.creativeitem.com
-*/
